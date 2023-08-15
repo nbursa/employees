84f4f6050d3f35ef36948f8f1d13e9a2b827c06a
Nenad Bursać <nbursa@gmail.com>
2023-08-15 12:03:35 +0200

    init axios & mui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'http://142.132.229.249:3000'
+    }
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@utils': '/src/utils',
+    }
+  }
+});
